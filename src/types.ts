b7173514987f838e2d27d22518b4fc5705120bf0
kota113 <kota113@keio.jp>
2024-08-28 15:03:55 +0800

    add restaurant interface

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,36 @@
+export interface RestaurantInformation {
+    budget: {
+        lunch: number
+        dinner: number
+    }
+    hasSmokingArea: boolean
+    openingTimeDesc: string
+    url: string
+    // array of payment methods
+    payments: Array<string>
+}
+
+export interface RestaurantItem {
+    id: string
+    name: string
+    shortDesc: string
+    longDesc: string
+    sumOfVacancy: number
+    // geopoint
+    location: {
+        latitude: number
+        longitude: number
+    }
+    address: string
+    // ラストオーダーの時間
+    lastOrderTimes: Array<number>
+    keywords: Array<string>
+    isPublished: boolean
+    // Firebase storageのURL
+    images: Array<string>
+    information: RestaurantInformation
+}
+
 export interface FirebaseTableType {
     id?: string
     lastUpdated: Date
@@ -16,8 +49,7 @@ export interface ReservationTablesItem {
 export interface ReservationItem {
     id: string;         //予約ID
     userName: string;  //予約を行ったユーザー名
-    // todo: レストランオブジェクトを定義する
-    restaurant?: object; //レストランオブジェクト (未定義)
+    restaurant?: RestaurantItem | string; //レストランオブジェクト、もしくはreference
     time: Date;         //予約日時
     tables: ReservationTablesItem[]; // 予約テーブル
     peopleCount: number; // 人数
